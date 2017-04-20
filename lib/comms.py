7a34cd7cbd261a844ec7ea0340f391847aad9246
Chh-Huang <chua5342@uni.sydney.edu.au>
2017-04-21 09:46:30 +1000

    Add files via upload

--- lib/comms.py
+++ lib/comms.py
@@ -38,24 +38,30 @@ class StealthConn(object):
         iv_string = "{:016b}".format(random.getrandbits(AES.block_size))
         iv = bytes(iv_string, "ascii")
         # Chosen AES as cipher in Cipher Feedback mode with first half of shared hash as 32 bytes key
-        self.cipher = AES.new(shared_hash[:32], AES.MODE_CFB, iv)
+        self.cipher = AES.new(shared_hash[:32], AES.MODE_CBC, iv)
         # Initialise HMAC with second half of shared hash as the secret
         self.hmac = HMAC.new(str(shared_hash[33:]).encode("ascii"), digestmod=SHA256)
 
 
     def send(self, data):
+        # Define padding funtion for CBC mode
+        size_to_add = lambda s: AES.block_size - len(s) % AES.block_size
+        pad = lambda s: s + (size_to_add(s)) * chr(size_to_add(s))
         # Randomly generate one-time session token to prevent replay attack when cipher and HMAC exist
         if self.cipher and not(self.token):
             self.token = SHA256.new(bytes(str(rd.getrandbits(16)), "ascii")).hexdigest().encode("ascii")
         if self.cipher:
             # Update HMAC with session token and data
             self.hmac.update(self.token)
-            if not isinstance(data, bytes):
-                data = bytes(str(data), "ascii")
-            self.hmac.update(data)
+            # Pad data to be sent
+            if isinstance(data, bytes):
+                data_to_sent = bytes(pad(data.decode("ascii")),"ascii")
+            else:
+                data_to_sent = bytes(pad(str(data)), "ascii")
+            self.hmac.update(data_to_sent)
             # Encrypted token and data to be sent
             encrypted_token = self.cipher.encrypt(self.token)
-            encrypted_data = self.cipher.encrypt(data)
+            encrypted_data = self.cipher.encrypt(data_to_sent)
             if self.verbose:
                 print("Original data: {}".format(data))
                 print("Encrypted data: {}".format(repr(encrypted_data)))
@@ -76,10 +82,16 @@ class StealthConn(object):
         self.conn.sendall(encrypted_data)
 
     def recv(self):
+        # Define unpadding function for CBC mode
+        unpad = lambda s: s[0:-ord(s[-1])]
         # Extract encrypted session token for all messages when cipher and HMAC exist
         if self.cipher:
-            # Receive and decrypt token
             token_len_packed = self.conn.recv(struct.calcsize('H'))
+            # Connection lost handling, stop the program when nothing can be received
+            if token_len_packed == b'':
+                # Close connection when lost connection
+                self.conn.close()
+                return
             token_len_unpacked = struct.unpack('H', token_len_packed)
             token_len = token_len_unpacked[0]
             encrypted_token = self.conn.recv(token_len)
@@ -100,10 +112,11 @@ class StealthConn(object):
         unpacked_contents = struct.unpack('H', pkt_len_packed)
         pkt_len = unpacked_contents[0]
         encrypted_data = self.conn.recv(pkt_len)
-
         if self.cipher:
             data = self.cipher.decrypt(encrypted_data)
             self.hmac.update(data)
+            # Unpad data
+            data = bytes(unpad(data.decode("ascii")), "ascii")
             # Check HMAC in case of tampering with messages
             assert(self.hmac.hexdigest() == hmac_digest.decode("ascii"))
             # Check session token to prevent replay attack
